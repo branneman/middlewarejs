081de3fb0750acc399ca7a5150bb671322a9463a
Bran van der Meer <branmovic@gmail.com>
2017-06-12 20:21:10 +0200

    test: Added tests for common use-case: guarded middlewares

--- index.test.js
+++ index.test.js
@@ -5,6 +5,7 @@ describe('MiddlewareJS', function() {
     describe('exports', function() {
 
         it('can be required', function() {
+
             var path = require('path');
             delete require.cache[path.resolve('./index.js')];
 
@@ -52,6 +53,72 @@ describe('MiddlewareJS', function() {
             function cb() {
                 expect(fn1).toHaveBeenCalled();
                 expect(fn2).toHaveBeenCalled();
+                done();
+            }
+        });
+
+    });
+
+    describe('middleware.use()', function() {
+
+        var middleware = require('.');
+
+        var guard = function(prefix, fn) {
+            return function(req, res, next) {
+                if (req.url === prefix) {
+                    fn(req, res, next);
+                } else {
+                    next();
+                }
+            };
+        };
+
+        it('guarded handlers: executes correct guard', function(done) {
+
+            var fn1Called = false;
+            var fn2Called = false;
+            var fn1 = function(a1, a2, next) {
+                fn1Called = true;
+                next();
+            };
+            var fn2 = guard('/prefix', function(a1, a2, next) {
+                fn2Called = true;
+                next();
+            });
+
+            var app = middleware();
+            app.use(fn1);
+            app.use(fn2);
+            app.run({ url: '/prefix' }, {}, cb);
+
+            function cb() {
+                expect(fn1Called).toBe(true);
+                expect(fn2Called).toBe(true);
+                done();
+            }
+        });
+
+        it('guarded handlers: skips incorrect guard', function(done) {
+
+            var fn1Called = false;
+            var fn2Called = false;
+            var fn1 = guard('/yes', function(a1, a2, next) {
+                fn1Called = true;
+                next();
+            });
+            var fn2 = guard('/no', function(a1, a2, next) {
+                fn2Called = true;
+                next();
+            });
+
+            var app = middleware();
+            app.use(fn1);
+            app.use(fn2);
+            app.run({ url: '/yes' }, {}, cb);
+
+            function cb() {
+                expect(fn1Called).toBe(true);
+                expect(fn2Called).toBe(false);
                 done();
             }
         });
